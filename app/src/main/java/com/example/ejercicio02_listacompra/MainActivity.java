22ae73fc4accdb11dda5d2d8b8bc50e593753267
guillem.burnlees.viada <guillem.burnlees.viada@outlook.es>
2019-09-24 14:26:18 +0200

    visual update colors

--- app/src/main/java/com/example/ejercicio02_listacompra/MainActivity.java
+++ app/src/main/java/com/example/ejercicio02_listacompra/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.invoke.ConstantCallSite;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnMercadonAdd, btnConsumAdd, btnDiaAdd;
@@ -26,9 +28,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MercadonaAddActivity.class);
-
                 startActivityForResult(intent, MERCADONA);
 
+            }
+        });
+
+        btnConsumAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ConsumAddActivity.class);
+                startActivityForResult(intent, CONSUM);
+
+            }
+        });
+
+        btnDiaAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DiaAddActivity.class);
+                startActivityForResult(intent, DIA);
             }
         });
 
